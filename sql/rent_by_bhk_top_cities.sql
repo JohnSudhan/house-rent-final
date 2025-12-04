00e3e892731d6cb594e4656eaac026f68e06fbf9
JohnSudhan <sudhansud30@gmail.com>
2025-12-04 19:47:46 +0100

    Add rent_by_bhk_top_cities.sql

---
+++ sql/rent_by_bhk_top_cities.sql
@@ -0,0 +1,19 @@
+-- rent_by_bhk_top_cities.sql
+WITH top_cities AS (
+    SELECT City
+    FROM cleaned_house_rent
+    GROUP BY City
+    ORDER BY COUNT(*) DESC
+    LIMIT 6
+)
+
+SELECT
+    c.City,
+    c.BHK,
+    COUNT(*) AS listings,
+    ROUND(AVG(c.Rent), 2) AS avg_rent
+FROM cleaned_house_rent c
+JOIN top_cities t
+    ON c.City = t.City
+GROUP BY c.City, c.BHK
+ORDER BY c.City, c.BHK;
