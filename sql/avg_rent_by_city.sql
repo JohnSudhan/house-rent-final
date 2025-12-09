cdefae88a7f419d3c7a605cfd99bffcf29ed1c05
JohnSudhan <sudhansud30@gmail.com>
2025-12-09 21:54:33 +0100

    Update avg_rent_by_city.sql

--- sql/avg_rent_by_city.sql
+++ sql/avg_rent_by_city.sql
@@ -1,9 +1,8 @@
--- avg_rent_by_city.sql
 SELECT
     City,
     COUNT(*) AS num_listings,
     ROUND(AVG(Rent), 2) AS avg_rent,
-    ROUND(PERCENTILE_APPROX(Rent, 0.5), 2) AS median_rent
+    ROUND(percentile_approx(Rent, 0.5), 2) AS median_rent
 FROM cleaned_house_rent
 GROUP BY City
 ORDER BY avg_rent DESC;
